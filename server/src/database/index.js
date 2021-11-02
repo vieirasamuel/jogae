6d76ea21dfd7a548f2cff3e72bd669ecfe548e15
Samuel Vieira <vieira.samuel@outlook.com.br>
2021-11-02 20:54:05 -0300

    adicionado teste de conexao ao banco de dados

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -7,4 +7,15 @@ const connection = new Sequelize(dbConfig);
 
 User.init(connection);
 
+async function testConnection() {
+  try {
+    await connection.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testConnection();
+
 module.exports = connection;
