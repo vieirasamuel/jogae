9fbca0b4726ed14c57f01add215889006cfe3b73
Samuel Vieira <vieira.samuel@outlook.com.br>
2021-12-01 19:34:42 -0300

    request de eventos na api

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import CameraIcon from '@mui/icons-material/PhotoCamera';
@@ -31,22 +31,31 @@ function Copyright() {
   );
 }
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#3f51b5"
-      },
-      secondary: {
-        main: "#f50057"
-
-      }
-    }
-  });
+  palette: {
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
 
 export default function Album() {
+  const [eventos, setEventos] = useState([]);
+
+  useEffect(async () => {
+    const { data } = await API.get('/events/list', {
+      headers: {
+        'x-access-token':
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZmQ0Yzk1ZTctYjk0Mi00NmRjLWIxYjItNDcwNmM2M2JlOTA5IiwibmFtZSI6IkNpY2xhbm8iLCJlbWFpbCI6ImNpY2xhbm8yQGVtYWlsLmNvbSIsImlhdCI6MTYzODM5NzQ3MSwiZXhwIjoxNjM4NDAxMDcxfQ.F7YvEMkqGBnofffoRA3Omt4cV9L9mlbvGRX0mVJ3bLM',
+      },
+    });
+    setEventos(data);
+  }, []);
+
+  console.log(eventos);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -77,25 +86,33 @@ export default function Album() {
             >
               Explorar
             </Typography>
-            
+
             <Stack
               sx={{ pt: 4 }}
               direction="row"
               spacing={2}
               justifyContent="center"
             >
-            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Procure novas quadras..."></input>
-
+              <input
+                type="text"
+                id="myInput"
+                onkeyup="myFunction()"
+                placeholder="Procure novas quadras..."
+              ></input>
             </Stack>
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+            {eventos.map((evento) => (
+              <Grid item key={evento.uuid} xs={12} sm={6} md={4}>
                 <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  sx={{
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
                 >
                   <CardMedia
                     component="img"
@@ -108,7 +125,7 @@ export default function Album() {
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Quadra Poliesportiva
+                      {evento.nome}
                     </Typography>
                   </CardContent>
                   <CardActions>
@@ -122,14 +139,13 @@ export default function Album() {
       </main>
       {/* Footer */}
       <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        
         <Typography
           variant="subtitle1"
           align="center"
           color="text.secondary"
           component="p"
         >
-        <Button size="small">Ver Mais</Button>
+          <Button size="small">Ver Mais</Button>
         </Typography>
         {/*<Copyright /> */}
       </Box>
