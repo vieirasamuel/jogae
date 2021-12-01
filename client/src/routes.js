d47d2d4fb76997749c5d51867895771a7e96fea5
Murilo <murilobritto96@gmail.com>
2021-12-01 16:54:31 -0300

    implementacoes das telas de forma de pagamento,checkout e cadastro

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,13 +4,19 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 // imports das páginas
 import Login from './pages/Login';
 import Feed from './pages/Feed';
+import Cadastro from './pages/Cadastro';
+import Checkout from './pages/Checkout';
+import FormaPagamento from './pages/FormaPagamento'
+
 const RoutesIndex = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Login />} path="/" exact />
         <Route element={<Feed />} path="/feed" />
-
+        <Route element={<Cadastro />} path="/cadastro" />
+        <Route element={<Checkout />} path="/checkout" />
+        <Route element={<FormaPagamento />} path="/formaPagamento" />
       </Routes>
     </BrowserRouter>
   );
