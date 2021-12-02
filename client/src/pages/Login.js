a93ebd0a6ce2bac1ad4ca47a444cd40497e4b680
Samuel Vieira <vieira.samuel@outlook.com.br>
2021-12-02 17:01:05 -0300

    refactor(login): alteracoes nos links e ajustes nos textos da pagina

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,8 @@
-import * as React from 'react';
+import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -15,10 +13,15 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="/">
+        Jogae
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -35,7 +38,7 @@ export default function SignUp() {
     // eslint-disable-next-line no-console
     console.log({
       email: data.get('email'),
-      password: data.get('password'),
+      password: data.get('senha'),
     });
   };
 
@@ -55,14 +58,17 @@ export default function SignUp() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Pagina Inicial
+            Faça login no Jogae
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-              </Grid>
+              <Grid item xs={12} sm={6}></Grid>
+              <Grid item xs={12} sm={6}></Grid>
               <Grid item xs={12}>
                 <TextField
                   required
@@ -77,15 +83,14 @@ export default function SignUp() {
                 <TextField
                   required
                   fullWidth
-                  name="Senha"
+                  name="senha"
                   label="Senha"
-                  type="Senha"
-                  id="Senha"
-                  autoComplete="new-password"
+                  type="password"
+                  id="senha"
+                  autoComplete="senha"
                 />
               </Grid>
-              <Grid item xs={12}>
-              </Grid>
+              <Grid item xs={12}></Grid>
             </Grid>
             <Button
               type="submit"
@@ -97,8 +102,8 @@ export default function SignUp() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
-                 Não tem conta? Cadastre-se!
+                <Link href="/cadastro" variant="body2">
+                  Não tem conta? Cadastre-se!
                 </Link>
               </Grid>
             </Grid>
