94d94840c08d0f6eea85fa29ce2c289017509f26
Murilo <murilobritto96@gmail.com>
2021-12-03 01:08:20 -0300

    alteracao da tela de DescricaoEvento e header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
 
 export default function Feed() {
   return (
@@ -11,7 +12,25 @@ export default function Feed() {
           <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
             Jogae
           </Typography>
-          <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+          <nav>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="/feed"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Inicio
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="/novoEvento"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Novo Evento
+            </Link>
+          </nav>
+          <Button href="/" variant="error" sx={{ my: 1, mx: 1.5 }}>
             Sair
           </Button>
         </Toolbar>
