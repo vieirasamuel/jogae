d75d315b329c9db0e30f652b2aba6b03322aa6e1
Samuel Vieira <vieira.samuel@outlook.com.br>
2021-12-03 01:19:36 -0300

    refactor(header): alteracao de cor nos botoes do header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,31 +9,36 @@ export default function Feed() {
   return (
     <AppBar position="relative">
       <Toolbar sx={{ flexWrap: 'wrap' }}>
-          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-            Jogae
-          </Typography>
-          <nav>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="/feed"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Inicio
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="/novoEvento"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Novo Evento
-            </Link>
-          </nav>
-          <Button href="/" variant="error" sx={{ my: 1, mx: 1.5 }}>
-            Sair
-          </Button>
-        </Toolbar>
+        <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+          Jogae
+        </Typography>
+        <nav>
+          <Link
+            color="white"
+            underline="hover"
+            href="/feed"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Inicio
+          </Link>
+          <Link
+            color="white"
+            underline="hover"
+            href="/novoEvento"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Novo Evento
+          </Link>
+        </nav>
+        <Button
+          href="/"
+          color="secondary"
+          variant="outlined"
+          sx={{ my: 1, mx: 1.5 }}
+        >
+          Sair
+        </Button>
+      </Toolbar>
     </AppBar>
   );
 }
